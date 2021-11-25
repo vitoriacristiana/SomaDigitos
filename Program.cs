bde52bbb595ae8e4f6478750f858e382625090ec
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-11-25 17:32:44 -0300

    Resolução exercício SomaDigitos

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SomaDigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            soma  int = 0;
+           Console . Write ( " Digite um número positivo inteiro: " );
+            char [] caracteres  =  Console . ReadLine (). ToCharArray ();
+
+             int [] digitos  =  retornaListaDigitos ( caracteres );
+
+            Console . Write ( " Digitos: " );
+
+            for ( int  i  =  0 ; i  <  caracteres.Length; i ++ )
+            {
+                Console . Write ($" { digitos [ i ]} " );
+
+              soma + = digitos [ i ];
+              
+            }
+            Console . WriteLine ($" \nSoma= {soma}");
+        }
+
+        static  int [] retornaListaDigitos ( char [] entrada )
+        {
+            int [] digitos  =  new  int [ entrada.Length ];
+
+            for ( int  i  =  0; i<  entrada.Length; i ++ )
+            {
+                digitos [ i ] =  entrada [ i ] -  '0';
+            }
+
+            return  digitos ;
+        
+        }
+    }
+}
